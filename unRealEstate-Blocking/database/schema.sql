ef18ff250e56e9780aba681219ba1ef73bc00c3f
Hossein Sadeghi <hossein.sadeghir@gmail.com>
2023-09-17 11:51:41 +0200

    feat(blocking): add database schema

---
+++ unRealEstate-Blocking/database/schema.sql
@@ -0,0 +1,31 @@
+USE `unreal_estate`;
+
+CREATE TABLE `agent`
+(
+    `id`         int(11)      NOT NULL AUTO_INCREMENT,
+    `firstname`  varchar(255) NOT NULL,
+    `lastname`   varchar(255) NOT NULL,
+    `nickname`   varchar(255) NOT NULL,
+    `email`      varchar(255) NOT NULL,
+    `phone`      varchar(255) NOT NULL,
+    `password`   varchar(255) NOT NULL,
+    `birth_date` timestamp    NOT NULL,
+    `created_at` timestamp    NOT NULL,
+    `updated_at` timestamp    NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `property`
+(
+    `id`          int(11)      NOT NULL AUTO_INCREMENT,
+    `owner_name`  varchar(255) NOT NULL,
+    `state`       varchar(255) NOT NULL,
+    `city`        varchar(255) NOT NULL,
+    `address`     varchar(500) NOT NULL,
+    `price`       int(11)      NOT NULL,
+    `description` text         NOT NULL,
+    `agent_id`    int(11)      NOT NULL REFERENCES agent (id),
+    `created_at`  timestamp    NOT NULL,
+    `updated_at`  timestamp    NOT NULL,
+    PRIMARY KEY (`id`)
+);
