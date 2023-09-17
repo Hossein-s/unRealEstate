eb015827f59876fce25755a20bf04dd39379d652
Hossein Sadeghi <hossein.sadeghir@gmail.com>
2023-09-17 12:51:31 +0200

    feat(blocking): add swagger ui

--- unRealEstate-Blocking/build.gradle.kts
+++ unRealEstate-Blocking/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
