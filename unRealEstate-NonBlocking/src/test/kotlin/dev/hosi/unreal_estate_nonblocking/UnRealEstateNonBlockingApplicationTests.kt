bb16c7f6d5a7a099cba7ba1cfe53d054d9318a28
Hossein Sadeghi <hossein.sadeghir@gmail.com>
2023-09-17 12:58:19 +0200

    chore(nonblocking): initialize non-blocking project

---
+++ unRealEstate-NonBlocking/src/test/kotlin/dev/hosi/unreal_estate_nonblocking/UnRealEstateNonBlockingApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hosi.unreal_estate_nonblocking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnRealEstateNonBlockingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
